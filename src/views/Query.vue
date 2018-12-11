135a65ee992075844d8c995b4eccc2a95fcffe2c
bart <bart.delrue@digipolis.gent>
2018-12-11 17:42:40 +0100

    Added yasqe.

---
+++ src/views/Query.vue
@@ -0,0 +1,34 @@
+<template>
+    <section class="detail-layout">
+        <h1>Query editor</h1>
+        <div id="yasqe"></div>
+    </section>
+</template>
+
+<script>
+
+  import YASQE from 'yasgui-yasqe'
+
+  export default {
+    name: 'home',
+    data () {
+      return {
+        yasqe: null
+      }
+    },
+    mounted () {
+      this.yasqe = YASQE(document.querySelector('#yasqe'), {
+        sparql: {
+          showQueryButton: true,
+          endpoint: 'https://qa.stad.gent/sparql'
+        }
+      })
+    }
+  }
+</script>
+
+<style scoped>
+    #yasqe {
+        font-size: .6rem;
+    }
+</style>
