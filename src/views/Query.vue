2b53566c053dc82b4c4d8848454efbe282464dec
bart <bart.delrue@digipolis.gent>
2018-12-12 15:22:57 +0100

    Added yasr.

--- src/views/Query.vue
+++ src/views/Query.vue
@@ -2,33 +2,34 @@
     <section class="detail-layout">
         <h1>Query editor</h1>
         <div id="yasqe"></div>
+        <div id="yasr"></div>
     </section>
 </template>
 
 <script>
 
-  import YASQE from 'yasgui-yasqe'
+  import YASQE from 'yasgui-yasqe/dist/yasqe.bundled'
+  import YASR from 'yasgui-yasr/dist/yasr.bundled'
 
   export default {
     name: 'home',
-    data () {
-      return {
-        yasqe: null
-      }
-    },
     mounted () {
-      this.yasqe = YASQE(document.querySelector('#yasqe'), {
+      let yasqe = YASQE(document.querySelector('#yasqe'), {
         sparql: {
           showQueryButton: true,
           endpoint: 'https://qa.stad.gent/sparql'
         }
       })
+      let yasr = YASR(document.querySelector('#yasr'), {
+        useGoogleCharts: false,
+        getUsedPrefixes: yasqe.getPrefixesFromQuery,
+        outputPlugins:  ['error', 'boolean', 'rawResponse', 'table', 'pivot'],
+        output: "table"
+      })
+      yasqe.options.sparql.callbacks.complete = yasr.setResponse
     }
   }
 </script>
 
-<style scoped>
-    #yasqe {
-        font-size: .6rem;
-    }
+<style>
 </style>
