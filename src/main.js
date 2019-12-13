78d86827e6444fe4b23013aa8837f16eabe54ef7
bart <bart.delrue@digipolis.gent>
2019-12-13 17:02:31 +0100

    Added vue-analytics

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+
+/**
+ * Google analytics
+ */
+Vue.use(VueAnalytics, {
+  id: 'UA-791237-65',
+  router
+})
 
 Vue.filter('truncate', (text, length, clamp) => {
   if (!text) {
