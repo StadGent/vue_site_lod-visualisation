89bca6cca34315653d98effd1743963eea6b347c
bart <bart.delrue@digipolis.gent>
2020-04-01 13:54:34 +0200

    Updated header template.

--- src/components/molecules/menu.vue
+++ src/components/molecules/menu.vue
@@ -1,27 +1,46 @@
 <template>
     <nav class="menu" v-if="routes">
-        <button class="toggle">Menu</button>
-        <div class="overlay"></div>
-        <div class="drawer" tabindex="-1">
-            <div class="header">
-                <a href="#" title="Home" class="site-logo " rel="home"></a>
-                <button class="close">Menu sluiten</button>
+        <button type="button" class="toggle modal-trigger" aria-expanded="false" aria-controls="menu">menu</button>
+        <ul>
+            <li v-for="(route, index) in routes" :key="index">
+                <router-link :to="route"
+                             active-class="active"
+                             :exact="route.path === '' || route.path === '/'"
+                             @click.native="menu.close()">{{ route.meta.menu }}
+                </router-link>
+            </li>
+        </ul>
+        <div id="menu" ref="modal" class="modal has-custom-binding menu" role="dialog" aria-modal="true" tabindex="-1">
+            <div class="modal-inner">
+                <div class="modal-header">
+                    <button type="button" class="button close icon-cross modal-close" data-target="menu">
+                        <span>sluiten</span>
+                    </button>
+                </div>
+                <div class="modal-content">
+                    <div class="header">
+                        <router-link :to="'/'" title="Home" class="site-logo" rel="home" />
+                    </div>
+                    <div class="menu-links">
+                        <ul>
+                            <li v-for="(route, index) in routes" :key="index">
+                                <router-link :to="route"
+                                             active-class="active"
+                                             :exact="route.path === '' || route.path === '/'"
+                                             @click.native="menu.close()">{{ route.meta.menu }}
+                                </router-link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <ul class="links">
-                <li v-for="(route, index) in routes" :key="index">
-                    <router-link :to="route"
-                                 active-class="active"
-                                 :exact="route.path === '' || route.path === '/'"
-                                 @click.native="menu.close()">{{ route.meta.menu }}
-                    </router-link>
-                </li>
-            </ul>
+            <div class="modal-overlay modal-close" data-target="menu" tabindex="-1"></div>
         </div>
     </nav>
 </template>
 
 <script>
-  import * as Menu from '../../assets/styleguide_override/menu.functions';
+  import Modal from '@digipolis-gent/modal'
 
   export default {
     data () {
@@ -35,7 +54,7 @@
       }
     },
     mounted () {
-      this.menu = new Menu(this.$el)
+      this.menu = new Modal(this.$refs.modal)
     }
   }
 </script>
